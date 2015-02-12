79365c084b7b783abdf6b2d53780a2ca0406ecce
sasezaki <sasezaki@gmail.com>
2015-02-12 23:24:25 +0900

    describe demo script

--- demos/test.php
+++ demos/test.php
@@ -1,6 +1,4 @@
 <?php
-use Phly\Http\Stream;
-
 require_once __DIR__.'/../vendor/autoload.php';
 
 $view = new SfpStreamView\View('template.phtml', __DIR__);
@@ -14,5 +12,5 @@ $fp = fopen("php://temp/maxmemory:". 1 * 1024 * 1024, 'wb+');
 $fp = $view->render($fp);
 
 rewind($fp); fpassthru($fp);
-var_dump(memory_get_peak_usage(true));
-echo `ls -la /tmp`;
+var_dump(memory_get_peak_usage(true)); // int(5505024) 5.25MB
+echo `ls -la /tmp`; // you can find 10MB temp file
